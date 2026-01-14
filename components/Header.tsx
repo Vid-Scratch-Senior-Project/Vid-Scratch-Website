b00c71ddff6eb6c0fc4af132a0793ff3bd41bff2
Karczel <92095164+Karczel@users.noreply.github.com>
2026-01-14 13:55:51 +0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,60 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <header>
-            <div>
-                Project Vid-Scratch
+            <div
+                style={{
+                    fontFamily: 'var(--font-roboto)',
+                    fontWeight: 780,
+                    fontSize: '24px',
+                    textTransform: 'uppercase',
+                    display: 'flex',
+                    gap: 8
+                }}>
+                Project <div
+                    style={{
+                        color: 'var(--secondary-text-color)',
+                        fontStyle: 'italic'
+                    }}>
+                    Vid-Scratch
+                </div>
             </div>
             <div
-            style={{
-                display: 'flex',
-                gap: 20
-            }}>
-                <div>
-                    Home
-                </div>
-                <div>
-                    What is Vid Scratch
-                </div>
+                style={{
+                    display: 'flex',
+                    gap: 20
+                }}>
+                <Link href="/" >
+                    <div
+                        style={{
+                            fontFamily: 'var(--font-roboto)',
+                            fontWeight: 780,
+                            fontSize: '24px',
+                            color: 'var(--secondary-text-color)'
+                        }}>
+                        Home
+                    </div>
+
+                </Link>
+                <Link href="/about-vid-scratch">
+                    <div
+                        style={{
+                            fontFamily: 'var(--font-roboto)',
+                            fontSize: '24px',
+                        }}>
+                        What is Vid Scratch
+                    </div>
+
+                </Link>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLScQKhhrPqNswvNk9kTlSj2izsN2Axk_Sp7wLa8ebEefPuSP4g/viewform?usp=dialog">
-                    Feedback
+                    <div
+                        style={{
+                            fontFamily: 'var(--font-roboto)',
+                            fontSize: '24px',
+                        }}>
+                        Feedback
+                    </div>
                 </a>
             </div>
         </header>
