0ed93e1e6d4396d3676a0909aea1a36a406c83fe
Karczel <92095164+Karczel@users.noreply.github.com>
2026-02-16 23:26:49 +0700

    Update Header.tsx
    menu style path dynamic

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+    const pathname = usePathname()
+
+    console.log(pathname)
     return (
         <header>
             <div
@@ -29,23 +35,40 @@ export default function Header() {
                     <div
                         style={{
                             fontFamily: 'var(--font-roboto)',
-                            fontWeight: 780,
                             fontSize: '24px',
-                            color: 'var(--secondary-text-color)'
+                            ...(pathname === '/' && {
+                                fontWeight: 780,
+                                color: 'var(--secondary-text-color)'
+                            })
                         }}>
                         Home
                     </div>
-
                 </Link>
                 <Link href="/about-vid-scratch">
                     <div
                         style={{
                             fontFamily: 'var(--font-roboto)',
                             fontSize: '24px',
+                            ...(pathname === '/about-vid-scratch' && {
+                                fontWeight: 780,
+                                color: 'var(--secondary-text-color)'
+                            })
                         }}>
                         What is Vid Scratch
                     </div>
-
+                </Link>
+                <Link href="/demo">
+                    <div
+                        style={{
+                            fontFamily: 'var(--font-roboto)',
+                            fontSize: '24px',
+                            ...(pathname === '/demo' && {
+                                fontWeight: 780,
+                                color: 'var(--secondary-text-color)'
+                            })
+                        }}>
+                        Demo
+                    </div>
                 </Link>
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLScQKhhrPqNswvNk9kTlSj2izsN2Axk_Sp7wLa8ebEefPuSP4g/viewform?usp=dialog">
                     <div
